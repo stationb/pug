2f7c1196ea1541870c90cb17d998aa9522ff2d86
Manos Saratsis <manos.saratsis@gmail.com>
2017-10-29 20:36:40 -0700

    [Hotfix] Applying objectid logic to matching script

--- scripts/match_buildings_streets.py
+++ scripts/match_buildings_streets.py
@@ -2,37 +2,37 @@ import json
 from rtree.index import Index
 from shapely.geometry import asShape
 
-from pprint import pprint
+STREETS_FILE = '../../datasets/oakland/streets.geojson.jsonlines'
+BUILDINGS_FILE = '../../datasets/oakland/buildings.geojson.jsonlines'
 
 
 class StreetIndex(object):
-    def __init__(self, street_file):
+    def __init__(self, streets_file):
         self.idx = Index()
         self.metadata = {}
         with open(streets_file) as f:
-            for i, line in enumerate(f.readlines()):
+            for line in f.readlines():
                 street = json.loads(line)
-                self.metadata[i] = street['properties']
-                shapely_street = asShape(street['geometry'])
-                self.idx.insert(i, list(shapely_street.bounds))
+                street_id = street['properties']['objectid']
+                self.metadata[street_id] = street['properties']
+                street_shape = asShape(street['geometry'])
+                self.idx.insert(int(street_id), list(street_shape.bounds))
 
     def find_nearest_street(self, building):
-        print building['properties']['bldgid3']
         shapely_building = asShape(building['geometry'])
         coordinate_tuple = (
             float(shapely_building.centroid.coords.xy[0][0]),
             float(shapely_building.centroid.coords.xy[1][0])
         )
         street_id = list(self.idx.nearest(coordinate_tuple))[0]
-        return self.metadata[street_id]['name']
+        return str(street_id)
 
 
 if __name__ == '__main__':
-    streets_file = '../../datasets/oakland/streets.geojson.jsonlines'
-    buildings_file = '../../datasets/oakland/buildings.geojson.jsonlines'
-    street_idx = StreetIndex(streets_file)
-    with open(buildings_file) as f:
-        for i, line in enumerate(f.readlines()):
+    street_idx = StreetIndex(STREETS_FILE)
+    with open(BUILDINGS_FILE) as f:
+        for line in f.readlines():
             building = json.loads(line)
-            print street_idx.find_nearest_street(building)
-            print
+            building_id = building['properties']['objectid']
+            street_id = street_idx.find_nearest_street(building)
+            print 'Building %s is near street %s' % (building_id, street_id)
