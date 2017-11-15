c52a019c8aec0121822728d6d4f81e3b9de2de7c
Manos Saratsis <manos.saratsis@gmail.com>
2017-11-14 22:41:04 -0800

    [Hotfix] Getting intersecting streets right

--- scripts/street_index_operations.py
+++ scripts/street_index_operations.py
@@ -15,7 +15,25 @@ class StreetIndex(object):
                 street = json.loads(line)
                 street_id = street['properties']['id']
                 street_shape = asShape(street['geometry'])
-                self.idx.insert(int(street_id), list(street_shape.bounds))
+                for i in range(len(street_shape.geoms)):
+                    seg_id = self.encode_seg_id(i, street_id)
+                    self.idx.insert(seg_id, street_shape.geoms[i].coords[0])
+                    self.idx.insert(-seg_id, street_shape.geoms[i].coords[-1])
+
+        self.bb_idx = Index()
+        with open(streets_file) as f:
+            for line in f.readlines():
+                street = json.loads(line)
+                street_id = int(street['properties']['id'])
+                street_shape = asShape(street['geometry'])
+                self.bb_idx.insert(street_id, list(street_shape.bounds))
+
+    def encode_seg_id(self, i, street_id):
+        return i * 1000000 + int(street_id)
+
+    def decode_seg_id(self, seg_id):
+        i = abs(seg_id) / 1000000
+        return abs(seg_id) - i
 
     def find_nearest_street(self, building):
         building_shape = asShape(building['geometry'])
@@ -23,14 +41,17 @@ class StreetIndex(object):
             float(building_shape.centroid.coords.xy[0][0]),
             float(building_shape.centroid.coords.xy[1][0])
         )
-        street_id = list(self.idx.nearest(building_centroid))[0]
+        street_id = list(self.bb_idx.nearest(building_centroid))[0]
         return str(street_id)
 
     def find_connected_street(self, street):
         street_id = int(street['properties']['id'])
         street_shape = asShape(street['geometry'])
-        street_bounds = street_shape.bounds
-        street_ids = list(self.idx.intersection(street_bounds))
+        street_start = street_shape.geoms[0].coords[0]
+        street_end = street_shape.geoms[-1].coords[-1]
+        seg_ids = list(self.idx.intersection(street_start))
+        seg_ids += list(self.idx.intersection(street_end))
+        street_ids = set(map(self.decode_seg_id, seg_ids))
         if street_id in street_ids:
             street_ids.remove(street_id)
         return street_ids
@@ -42,7 +63,7 @@ if __name__ == '__main__':
         with open(BUILDINGS_FILE, 'r') as infile:
             for line in infile.readlines():
                 building = json.loads(line)
-                building_id = 'pug://building/%s' % building['properties']['id']
+                building_id = 'pug://building/%s' % building['properties']['id'].replace(' ', '_')
                 street_id = 'pug://street/%s' % street_idx.find_nearest_street(building)
                 outfile.write('<%s> <pug://rel/on> <%s> .' % (building_id, street_id))
                 outfile.write('\n')
@@ -55,7 +76,7 @@ if __name__ == '__main__':
                 connected_street_ids = street_idx.find_connected_street(street)
                 for csid in connected_street_ids:
                     csid = 'pug://street/%s' % csid
-                    outfile.write('<%s> <pug://rel/connects_to> <%s> .' % (street_id, csid))
+                    outfile.write('<%s> <pug://rel/intersects> <%s> .' % (street_id, csid))
                     outfile.write('\n')
-                    outfile.write('<%s> <pug://rel/connects_to> <%s> .' % (csid, street_id))
+                    outfile.write('<%s> <pug://rel/intersects> <%s> .' % (csid, street_id))
                     outfile.write('\n')
