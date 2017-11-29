b1fe0bc76b69ffabec5e88ca9b8c429abf5a4664
jerluc <jeremyalucas@gmail.com>
2017-11-28 20:21:20 -0800

    Adding sample Pyley script

---
+++ scripts/queries.py
@@ -0,0 +1,11 @@
+from pyley import CayleyClient, GraphObject
+
+
+if __name__ == '__main__':
+    client = CayleyClient()
+    g = GraphObject()
+
+    q = g.V('<pug://EARHART_RD_bldgL877>').All()
+
+    r = client.Send(q)
+    print(r.result)
