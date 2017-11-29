7ac79ca6a7081912f80a8f1f2c5a47a6976780b5
jerluc <jeremyalucas@gmail.com>
2017-11-28 20:34:48 -0800

    Adding `pug://type/<CLASS>` node props

--- scripts/add_node_props.py
+++ scripts/add_node_props.py
@@ -7,16 +7,20 @@ IRI_TEMPLATE = '<pug://%s>'
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('Usage: python %s <JSONLINES_FILE>' % sys.argv[0])
+    if len(sys.argv) < 3:
+        print('Usage: python %s <NODE_TYPE> <JSONLINES_FILE>' % sys.argv[0])
         sys.exit(255)
-    fn = os.path.abspath(sys.argv[1])
+    node_type = sys.argv[1]
+    node_type_iri = IRI_TEMPLATE % ('type/%s' % node_type)
+    fn = os.path.abspath(sys.argv[2])
 
     with open(fn) as f:
         for line in f:
             node = json.loads(line)
-            node_id = node['properties']['id'].replace(' ', '_')
+            node_id = '/'.join((node_type, node['properties']['id'].replace(' ', '_')))
             if node_id:
+                type_triple = IRI_TEMPLATE % node_id, IRI_TEMPLATE % 'rel/type', node_type_iri
+                print('%s %s %s' % type_triple)
                 for k, v in node['properties'].items():
                     rel = 'rel/%s' % k
                     if v:
