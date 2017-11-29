c61b3c971a9392403f883702b33b3fb06f0699aa
jerluc <jeremyalucas@gmail.com>
2017-11-28 19:45:08 -0800

    Migrate to use remote PGsql

--- db/run.sh
+++ db/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 docker run -v $PWD/cayley.yml:/etc/cayley.yml \
-           -v $PWD/data:/data \
            -p 64210:64210 \
            -d \
            --name pug-db \
--- db/stop.sh
+++ db/stop.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-set -e
-
 docker kill pug-db
 docker rm pug-db
